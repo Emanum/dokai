331712525155d12c2ec64e85b3ea88467a9c5f28
Manuel Eiweck <manuel.eiweck@bkf.at>
2024-03-07 23:05:16 +0100

    base project

---
+++ Readme.md
@@ -0,0 +1,79 @@
+# Cloud Version with Modal.com
+
+## Requirements
+
+- Python 3.10
+- Github Account linked to Modal.com (you get a 30 dollar free credits each month)
+
+## Components
+
+There are just two components for the cloud version
+* **modal-style-transfer.py** - Which is the actual pipeline that runs the stable diffusion pipeline and hosts a webserver to trigger the style transfer and static HTML files
+* **frontend/*** - The frontend files that are served by the webserver. A one-page HTML side with alpinejs and tailwindcss
+
+## Setup
+
+```bash
+pip install modal
+```
+
+Afterward, create a token
+
+```bash
+modal token new
+```
+
+Then you can either serve a dev version temporarily or deploy a stable version.
+
+***Dev Version***
+
+
+```bash
+modal serve dokai.py
+```
+
+***Stable Version***
+
+```bash
+modal deploy dokai.py
+```
+
+# Local Style Transfer
+
+## Requirements 
+
+Tested on:
+- Python 3.10
+- Win11
+- Nvidia RX3070
+
+Stable diffusion uses pytorch so a cuda enabled GPU and operating system is required.
+
+## Prepare Environment
+
+**Optional Use Miniconda** to create a virtual environment. See [here](https://docs.conda.io/projects/conda/en/latest/user-guide/install/index.html) for installation instructions.
+```bash
+conda create -n stable-style-transfer python=3.10
+conda activate table-style-transfer
+```
+
+**Install pipenv**
+```bash
+pip install pipenv
+```
+
+**Install dependencies**
+```bash
+pipenv install
+```
+
+**Install PyTorch**
+
+go to 
+https://pytorch.org/get-started/locally/
+and search for the latest stable version for your system
+then install it similar to this
+```bash
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+```
+
---
+++ dokai.py
@@ -0,0 +1,304 @@
+# ---
+# output-directory: "/tmp/stable-diffusion-xl"
+# args: ["--prompt", "An astronaut riding a green horse"]
+# runtimes: ["runc", "gvisor"]
+# ---
+# # Stable Diffusion XL 1.0
+#
+# This example is similar to the [Stable Diffusion CLI](/docs/examples/stable_diffusion_cli)
+# example, but it generates images from the larger SDXL 1.0 model. Specifically, it runs the
+# first set of steps with the base model, followed by the refiner model.
+#
+# [Try out the live demo here!](https://modal-labs--stable-diffusion-xl-app.modal.run/) The first
+# generation may include a cold-start, which takes around 20 seconds. The inference speed depends on the GPU
+# and step count (for reference, an A100 runs 40 steps in 8 seconds).
+
+# ## Basic setup
+
+from pathlib import Path
+
+from modal import Image, Mount, Stub, asgi_app, gpu, method
+
+
+# ## Define a container image
+#
+# To take advantage of Modal's blazing fast cold-start times, we'll need to download our model weights
+# inside our container image with a download function. We ignore binaries, ONNX weights and 32-bit weights.
+#
+# Tip: avoid using global variables in this function to ensure the download step detects model changes and
+# triggers a rebuild.
+
+
+def download_models():
+    from huggingface_hub import snapshot_download
+
+    # ignore = ["*.bin", "*.onnx_data", "*/diffusion_pytorch_model.safetensors"]
+    ignore = []
+    snapshot_download(
+        "naver-clova-ix/donut-base-finetuned-cord-v2", ignore_patterns=ignore
+    )
+    snapshot_download(
+        "naver-clova-ix/donut-base-finetuned-cord-v2", ignore_patterns=ignore
+    )
+
+    # snapshot_download(
+    #     "lllyasviel/control_v11f1p_sd15_depth", ignore_patterns=ignore
+    # )
+    # snapshot_download(
+    #     "Intel/dpt-hybrid-midas", ignore_patterns=ignore
+    # )
+
+    # custom models from civitai.com
+    # run_async(download_file_with_tqdm, "https://civitai.com/api/download/models/90072",
+    #           "photon_v1.safetensors")
+    # run_async(download_file_with_tqdm, "https://civitai.com/api/download/models/40816",
+    #           "aniflatmixAnimeFlatColorStyle_v20.safetensors")
+    # run_async(download_file_with_tqdm, "https://civitai.com/api/download/models/105924",
+    #           "cetusMix_Whalefall2.safetensors")
+    # run_async(download_file_with_tqdm, "https://civitai.com/api/download/models/76907",
+    #           "ghostmix_v20Bakedvae.safetensors")
+    # run_async(download_file_with_tqdm, "https://civitai.com/api/download/models/256869",
+    #           "majicmixFantasy_v30Vae.safetensors")
+    # run_async(download_file_with_tqdm, "https://civitai.com/api/download/models/274039",
+    #           "juggernaut_reborn.safetensors")
+    # run_async(download_file_with_tqdm, "https://civitai.com/api/download/models/256504",
+    #           "etherBluMix_etherBlueMix6.safetensors")
+
+def run_async(func, *args, **kwargs):
+    import threading
+    thread = threading.Thread(target=func, args=args, kwargs=kwargs)
+    thread.start()
+    return thread
+
+
+def download_file_with_tqdm(url, filename):
+    import requests
+    from pathlib import Path
+    from tqdm import tqdm
+
+    r = requests.get(url, allow_redirects=True, stream=True)
+    with open(filename, 'wb') as f:
+        file_size = int(r.headers['Content-Length'])
+        chunk_size = 1024
+        num_bars = int(file_size / chunk_size)
+        for chunk in tqdm(r.iter_content(chunk_size=chunk_size), total=num_bars, unit='KB', desc=filename, leave=True):
+            f.write(chunk)
+
+
+image = (
+    Image.debian_slim()
+    .apt_install(
+        "libglib2.0-0", "libsm6", "libxrender1", "libxext6", "ffmpeg", "libgl1"
+    )
+    .pip_install(
+        "diffusers",
+        "torch",
+        "torchvision",
+        "torchaudio",
+        "transformers",
+        "datasets",
+        # "invisible_watermark",
+        # "transformers",
+        # "accelerate",
+        # "safetensors",
+        # "accelerate",
+        # "opencv-python",
+        # "numpy",
+        # "omegaconf",
+        # "pillow"
+    )
+    .run_function(download_models)
+)
+
+stub = Stub("dokai", image=image)
+
+with image.imports():
+    import re
+    from transformers import DonutProcessor, VisionEncoderDecoderModel
+    # from datasets import load_dataset
+    import torch
+
+
+
+# ## Load model and run inference
+#
+# The container lifecycle [`__enter__` function](https://modal.com/docs/guide/lifecycle-functions#container-lifecycle-beta)
+# loads the model at startup. Then, we evaluate it in the `run_inference` function.
+#
+# To avoid excessive cold-starts, we set the idle timeout to 240 seconds, meaning once a GPU has loaded the model it will stay
+# online for 4 minutes before spinning down. This can be adjusted for cost/experience trade-offs.
+
+
+@stub.cls(gpu=gpu.A10G(), container_idle_timeout=240, concurrency_limit=5)
+class Model:
+    def __init__(self):
+        self.pipe = None
+
+    def __enter__(self):
+        import torch
+        import re
+
+        from transformers import DonutProcessor, VisionEncoderDecoderModel
+        import torch
+
+        processor = DonutProcessor.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
+        model = VisionEncoderDecoderModel.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
+
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        model.to(device)
+
+        # load_options = dict(
+        #     torch_dtype=torch.float16,
+        #     use_safetensors=True,
+        #     variant="fp16",
+        #     device_map="auto",
+        # )
+
+        # Load base model
+
+        # Load ControlNet
+        # print("Loading ControlNet Canny")
+        # self.canny_controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny",
+        #                                                         torch_dtype=torch.float16,
+        #                                                         use_safetensors=True)
+        #
+        # print("Loading ControlNet Depth")
+        # self.depth_controlnet = ControlNetModel.from_pretrained("lllyasviel/control_v11f1p_sd15_depth",
+        #                                                         torch_dtype=torch.float16,
+        #                                                         use_safetensors=True)
+
+        # print("Loading ControlNetXL Depth")
+        # self.depth_controlnetXL = ControlNetModel.from_pretrained("diffusers/controlnet-depth-sdxl-1.0",
+        #                                                         variant="fp16",
+        #                                                         use_safetensors=True,
+        #                                                         torch_dtype=torch.float16,
+        # #                                                         ).to("cuda")
+        # print("Loading ControlNet depth-estimation")
+        # self.depth_estimator = pipeline("depth-estimation", model="Intel/dpt-hybrid-midas")
+
+        # Compiling the model graph is JIT so this will increase inference time for the first run
+        # but speed up subsequent runs. Uncomment to enable.
+        # self.base.unet = torch.compile(self.base.unet, mode="reduce-overhead", fullgraph=True)
+        # self.refiner.unet = torch.compile(self.refiner.unet, mode="reduce-overhead", fullgraph=True)
+
+    @method()
+    def inference(self, prompt: str, init_image_bytes: bytes, guess_mode: bool = False, canny: bool = False,
+                  model_name: str = "aniflatmixAnimeFlatColorStyle_v20.safetensors", creativity: int = 50):
+        # negative_prompt = "disfigured, ugly, deformed"
+
+        # write image to file
+        with open("image.png", "wb") as f:
+            f.write(init_image_bytes)
+
+        init_image = load_image("image.png")
+        # # reduce image size to max 1024px on the longest side but keep aspect ratio
+        # max_size = 1024
+        # if init_image.width > max_size or init_image.height > max_size:
+        #     print(f"Resizing image from {init_image.width}x{init_image.height}")
+        #     if init_image.width > init_image.height:
+        #         new_width = max_size
+        #         new_height = int(max_size * init_image.height / init_image.width)
+        #     else:
+        #         new_height = max_size
+        #         new_width = int(max_size * init_image.width / init_image.height)
+        #     init_image = init_image.resize((new_width, new_height))
+        #     print(f"Resized image to {new_width}x{new_height}")
+
+        dataset = load_dataset("hf-internal-testing/example-documents", split="test")
+        image = dataset[2]["image"]
+
+        # prepare decoder inputs
+        task_prompt = "<s_cord-v2>"
+        decoder_input_ids = processor.tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
+
+        pixel_values = processor(image, return_tensors="pt").pixel_values
+
+        outputs = model.generate(
+            pixel_values.to(device),
+            decoder_input_ids=decoder_input_ids.to(device),
+            max_length=model.decoder.config.max_position_embeddings,
+            pad_token_id=processor.tokenizer.pad_token_id,
+            eos_token_id=processor.tokenizer.eos_token_id,
+            use_cache=True,
+            bad_words_ids=[[processor.tokenizer.unk_token_id]],
+            return_dict_in_generate=True,
+        )
+
+        sequence = processor.batch_decode(outputs.sequences)[0]
+        sequence = sequence.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
+        sequence = re.sub(r"<.*?>", "", sequence, count=1).strip()  # remove first task start token
+        print(processor.token2json(sequence))
+
+
+
+
+        # import io
+        #
+        # byte_stream = io.BytesIO()
+        # image.save(byte_stream, format="PNG")
+        # image_bytes = byte_stream.getvalue()
+        #
+        # return image_bytes
+
+
+# And this is our entrypoint; where the CLI is invoked. Explore CLI options
+# with: `modal run stable_diffusion_xl.py --prompt 'An astronaut riding a green horse'`
+
+
+@stub.local_entrypoint()
+def main(prompt: str, init_image: bytes, guess_mode: bool = False,
+         model_name: str = "aniflatmixAnimeFlatColorStyle_v20.safetensors"):
+    res = Model().inference.remote(prompt, init_image, guess_mode, model_name)
+
+    # dir = Path("output")
+    # if not dir.exists():
+    #     dir.mkdir(exist_ok=True, parents=True)
+
+    # output_path = dir / "output.png"
+    # print(f"Saving it to {output_path}")
+    print(res)
+
+
+# ## A user interface
+#
+# Here we ship a simple web application that exposes a front-end (written in Alpine.js) for
+# our backend deployment.
+#
+# The Model class will serve multiple users from a its own shared pool of warm GPU containers automatically.
+#
+# We can deploy this with `modal deploy stable_diffusion_xl.py`.
+
+frontend_path = Path(__file__).parent / "frontend"
+
+
+@stub.function(
+    mounts=[Mount.from_local_dir(frontend_path, remote_path="/assets")]
+)
+@asgi_app()
+def app():
+    import fastapi.staticfiles
+    from fastapi import FastAPI
+
+    web_app = FastAPI()
+
+    @web_app.post("/infer/")
+    async def infer(request: fastapi.Request):
+        from fastapi.responses import Response
+
+        form = await request.form()
+        init_image = await form["init_image"].read()
+        prompt = form["prompt"]
+        guess_mode = form["guess_mode"]
+        model_name = form["model_name"]
+        creativity = form["creativity"]
+        canny = form["canny"]
+
+        res = Model().inference.remote(prompt, init_image, guess_mode, canny, model_name, creativity)
+
+        return Response(res, media_type="text/plain")
+
+    web_app.mount(
+        "/", fastapi.staticfiles.StaticFiles(directory="/assets", html=True)
+    )
+
+    return web_app
